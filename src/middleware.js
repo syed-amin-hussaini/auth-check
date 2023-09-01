32ed10b9960d12dbd923b174f76bb050a2e03e56
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-01 09:45:37 +0500

    Changes in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -13,18 +13,22 @@ export default async function middleware(req) {
 
   try {
     const userIpResponse = await fetch("https://json.geoiplookup.io/", requestOptions);
-    const userIpData = await userIpResponse.json();
-    const userCountry = userIpData.country_name;
+    const userIpData = await userIpResponse?.json();
+    const userCountry = userIpData?.country_name ?? "empty";
     // console.log(userCountry)
     // const token = req.cookies.get("token");
-    let user = req.cookies.get("user")?.value;
+    let user = req?.cookies?.get("user")?.value;
      user = JSON?.parse(user)
     // console.log({req})
     console.log(user)
     console.log(user?.token)
    
-    if (userCountry !== 'Pakistan' && req.url !== '/testing') {
+    if (userCountry !== 'Pakistan' && req.url !== '/notallow') {
       console.log("Redirecting user from Pakistan to /testing route");
+      return NextResponse.rewrite(new URL('/notallow', req.url))
+    }
+    if (userCountry !== 'empty' && req.url !== '/testing') {
+      console.log("Redirecting user from Empty to /empty route");
       return NextResponse.rewrite(new URL('/testing', req.url))
     }
     if ( user?.profile_status != "complete"  ||  user?.profile_status == undefined || user?.profile_status == null ) {
