a0789dfeb96f33a560c1377846d700f8e36e8ee4
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-01 11:30:55 +0500

    revert code - 2

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -26,58 +26,58 @@ const nextAuthOptions = (req, res) => {
       strategy: "jwt",
     },
     secret: process.env.JWT_SECRET,
-    // callbacks: {
-    //   async signIn(user, account, profile) {
-    //     try {
-    //       // console.log({ user });
-    //       const apiUrl =  `${process.env.NEXT_PUBLIC_API_URL}verify-user`;
-    //       const requestData = {
-    //         id: user?.user?.id,
-    //         name: user?.user?.name,
-    //         image: user?.user?.image,
-    //         email: user?.user?.email,
-    //         source: user?.account?.provider,
-    //       };
+    callbacks: {
+      async signIn(user, account, profile) {
+        try {
+          // console.log({ user });
+          const apiUrl =  `${process.env.NEXT_PUBLIC_API_URL}verify-user`;
+          const requestData = {
+            id: user?.user?.id,
+            name: user?.user?.name,
+            image: user?.user?.image,
+            email: user?.user?.email,
+            source: user?.account?.provider,
+          };
 
-    //       const response = await axios.post(apiUrl, requestData);
-    //       // console.log(response?.data)
-    //       let token = response?.data?.token;
-    //       let profile_status = response?.data?.profile_status;
-    //       console.log("API REsult") 
-    //       console.log({ token })
-    //       console.log({profile_status})
+          const response = await axios.post(apiUrl, requestData);
+          // console.log(response?.data)
+          let token = response?.data?.token;
+          let profile_status = response?.data?.profile_status;
+          console.log("API REsult") 
+          console.log({ token })
+          console.log({profile_status})
 
-    //       // Use the useEffect hook to set the cookie on the client side
-    //       // useEffect(() => {
-    //         // setCookie({ res }, 'user', `{\"token\":\"${token}\",\"profile_status\":\"${profile_status}\"}` , {
-    //         setCookie({ res }, 'user', `{\"token\":\"${token}\",\"profile_status\":\"incomplete\"}` , {
-    //           maxAge: 3600, // Cookie expiration time in seconds (e.g., 1 hour)
-    //           path: '/',    // Cookie path
-    //         });
+          // Use the useEffect hook to set the cookie on the client side
+          // useEffect(() => {
+            // setCookie({ res }, 'user', `{\"token\":\"${token}\",\"profile_status\":\"${profile_status}\"}` , {
+            setCookie({ res }, 'user', `{\"token\":\"${token}\",\"profile_status\":\"incomplete\"}` , {
+              maxAge: 3600, // Cookie expiration time in seconds (e.g., 1 hour)
+              path: '/',    // Cookie path
+            });
 
-    //       console.log("Third-party API response:", response?.data);
-    //     } catch (error) {
-    //       console.error("API Error:", error);
-    //     }
+          console.log("Third-party API response:", response?.data);
+        } catch (error) {
+          console.error("API Error:", error);
+        }
 
-    //     return true;
-    //   },
-    //   async jwt({ token, user, account }) {
-    //     // console.log("jwt");
-    //     // console.log({ token });
+        return true;
+      },
+      async jwt({ token, user, account }) {
+        // console.log("jwt");
+        // console.log({ token });
 
-    //     if (token || user) {
-    //       token.userRole = "admin";
-    //       return { ...token };
-    //     }
-    //   },
-    //   // async redirect({url, baseUrl}) {
-    //   //   console.log('url', url);
-    //   //   console.log('baseUrl', baseUrl);
+        if (token || user) {
+          token.userRole = "admin";
+          return { ...token };
+        }
+      },
+      async redirect({url, baseUrl}) {
+        console.log('url', url);
+        console.log('baseUrl', baseUrl);
         
-    //   //   return url.startsWith(baseUrl) ? "/" : baseUrl + '/';
-    //   // }
-    // },
+        return url.startsWith(baseUrl) ? "/" : baseUrl + '/';
+      }
+    },
   };
 };
 
