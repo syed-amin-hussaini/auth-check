c974ae4388de2ecfcf91aa2b95618ec3c2896f94
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-05 10:24:06 +0500

    Chages to pwa

--- next.config.js
+++ next.config.js
@@ -1,48 +1,32 @@
-// const path = require("path");
-// const runtimeCaching = require("next-pwa/cache");
 
-// const withPWA = require("next-pwa")({
-//   dest: "public",
-// });
-
-// module.exports = withPWA({
-//   // next.js config
-// });
-// const hostnames = ["platform-lookaside.fbsbx.com", "lh3.googleusercontent.com"];
-
-// module.exports = {
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-//   images: {
-//     remotePatterns: hostnames.map((hostname) => ({
-//       protocol: "https",
-//       hostname,
-//     })),
-//   },
-// };
-
-// // module.exports = nextConfig;
-
-
-const path = require("path");
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 const runtimeCaching = require("next-pwa/cache");
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
-const hostnames = ["platform-lookaside.fbsbx.com", "lh3.googleusercontent.com"];
+module.exports = withPWA({
+  // next.js config
+})
 
 module.exports = {
-  ...withPWA, // Include the configuration from next-pwa
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
   images: {
-    remotePatterns: hostnames.map((hostname) => ({
-      protocol: "https",
-      hostname,
-    })),
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
   },
-};
+}
+
+// module.exports = nextConfig;
