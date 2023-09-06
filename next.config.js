02a6ca110216cdce36be4bfe2af53e7f5079370a
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-06 10:08:54 +0500

    Checking pwa -2

--- next.config.js
+++ next.config.js
@@ -1,30 +1,104 @@
-
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache.js')
 const path = require('path')
- 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+
+const redirects = async () => {
+  return [
+    {
+      source: '/_backups',
+      destination: '/',
+      permanent: true,
+    },
+  ]
 }
-const runtimeCaching = require("next-pwa/cache");
 
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const rewrites = async () => {
+  return [
+    {
+      source: '/rewrite',
+      destination: '/',
+    },
+  ]
+}
 
-module.exports = withPWA({
-  // next.js config
-})
+const headers = async () => {
+  return [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN'
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block'
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=63072000; includeSubDomains; preload'
+        },
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on'
+        }
+      ],
+    },
+  ]
+}
 
-module.exports = {
+
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+  swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
+    domains: ['localhost', 'image.shutterstock.com'],
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/_styles')],
+  },
+  webpack: (config) => {
+    // find the built-in loader
+    const imageLoaderRule = config.module.rules.find(
+      (rule) => rule.loader === 'next-image-loader'
+    )
+    // make the loader ignore *.inline files
+    imageLoaderRule.exclude = /\.inline\.(png|jpg|svg)$/i
+    
+    // add a new URL loader for *.inline files
+    config.module.rules.push({
+      test: /\.inline\.(png|jpg|gif)$/i,
+      use: [
+        {
+          loader: 'url-loader'
+        }
+      ]
+    })
+    return config
+  },
+  pwa: {
+    dest: 'public',
+    // swSrc: './service-worker.js',
+    // register: true,
+    // runtimeCaching,
+    // mode: 'production',
+    // reloadOnOnline: true,
+    // cacheOnFrontEndNav: true,
+    // disable: process.env.NODE_ENV === 'development',
+  }
 }
 
-// module.exports = nextConfig;
+module.exports = buildConfig = _phase => {
+  const plugins = [withPWA]
+  const config = plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig, headers, rewrites, redirects
+  })
+  return config
+}
