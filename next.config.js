c054ba5ef407a74e05aeb34d3ed71d29b340bf41
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-06 12:15:08 +0500

    Checking pwa -

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
-
-const path = require('path')
- 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
-const runtimeCaching = require("next-pwa/cache");
-
 const withPWA = require('next-pwa')({
   dest: 'public'
 })
 
 module.exports = withPWA({
-  // next.js config
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 })
-
-// module.exports = nextConfig;
