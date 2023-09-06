97a28a0057691f0b51c9406ece34aa6e8b1401a9
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-06 15:10:26 +0500

    Changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="manifest" href="/manifest.json?v=1" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
           <link
