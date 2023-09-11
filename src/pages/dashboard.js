44d13bf6a0d8c1641aa86a4cca9371fabbbcc1a7
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-11 12:38:09 +0500

    Changes test

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -54,14 +54,14 @@ export default function Home(props) {
 export async function getServerSideProps(context) {
   const session = await getSession(context)
   console.log({session})
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: '/',
+  //       permanent: false,
+  //     },
+  //   }
+  // }
 
   return {
     props: { session }
