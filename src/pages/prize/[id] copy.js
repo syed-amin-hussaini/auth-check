822acf08fe35272d5d03fbde10a3a1281a8d597c
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-14 17:23:39 +0500

    Change in Cookie scan

--- src/pages/prize/[id] copy.js
+++ src/pages/prize/[id] copy.js
@@ -1,48 +1,48 @@
 import Head from "next/head";
-// import styles from "../styles/Home.module.css";
-import { getSession, useSession } from "next-auth/react";
 import Nav from "@/components/Nav";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState, useRef } from "react";
 import Drawer from "@/components/Drawer";
-import Link from "next/link";
 import Webcam from "react-webcam";
-
 import styles from "@/src/styles/Camera.module.scss";
 import { useRouter } from "next/router";
 import axios from "axios";
-
-import { useTorchLight } from '@blackbox-vision/use-torch-light';
+import { useTorchLight } from "@blackbox-vision/use-torch-light";
 
 export default function Price() {
-  
   const { data: session, status } = useSession();
   const loading = status === "loading";
-
   const router = useRouter();
-  const { id } = router.query
+  const { id } = router.query;
+
+  const [torchEnabled, setTorchEnabled] = useState(false);
+  const [imgSrc, setImgSrc] = useState(null);
+  const webcamRef = useRef(null);
+
+  // Declare and initialize the streamRef
+  const streamRef = useRef(null);
+
+  // Toggle Torch
+  const [on, toggle] = useTorchLight();
+
+  const toggleTorch = () => {
+    setTorchEnabled(!torchEnabled);
+    toggle(streamRef.current); // Pass the streamRef to the toggle function
+  };
 
   const videoConstraints = {
     width: 500,
     height: 500,
     facingMode: "environment",
+    torch: torchEnabled,
   };
-
-  // Torch
-  const streamRef = useRef(null);
-
-  const [on, toggle] = useTorchLight(streamRef.current);
-
-  // 
-
-  const webcamRef = React.useRef(null);
-  const [imgSrc, setImgSrc] = React.useState(null);
 
   const capture = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
     let data_i = JSON.stringify({
-      img: imageSrc
+      img: imageSrc,
     });
+
     // Send the captured image to a third-party API
     try {
       const response = await axios({
@@ -51,16 +51,14 @@ export default function Price() {
         data: data_i,
         headers: { "Content-Type": "application/json" },
       });
-      
-      if (response.ok) {
-        console.log({response})
-        // Handle success
+
+      if (response.status === 200) {
+        console.log(response.data); // Handle success
       } else {
-        console.log("Else")
-        // Handle error
+        console.error("Error:", response.status, response.statusText); // Handle error
       }
     } catch (error) {
-      console.error('Error sending image:', error);
+      console.error("Error sending image:", error);
     }
   };
 
@@ -78,13 +76,21 @@ export default function Price() {
           ref={webcamRef}
           mirrored={false}
           videoConstraints={videoConstraints}
-          screenshotFormat="image/jpeg" 
+          screenshotFormat="image/jpeg"
           screenshotQuality={1}
-          />
+        />
         <button onClick={capture}>Capture photo {id}</button>
-        <button onClick={toggle} style={{marginBottom:"40px"}}>{on ? 'Disable Torch' : 'Enable Torch'}</button>
-        {imgSrc && <img style={{position: "absolute",left: "20%",top: "-20px"}} src={imgSrc} />}
+        <button style={{ marginBottom: "40px" }} onClick={toggleTorch}>
+          {torchEnabled ? "Disable Torch" : "Enable Torch"}
+        </button>
 
+        {imgSrc && (
+          <img
+            style={{ position: "absolute", left: "20%", top: "-20px" }}
+            src={imgSrc}
+            alt="captured image"
+          />
+        )}
       </main>
     </div>
   );
