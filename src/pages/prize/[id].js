64fea1a490477dfc91351f242eb0bf90f5dae5b9
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-18 18:32:30 +0500

    Scanner

--- src/pages/prize/[id].js
+++ src/pages/prize/[id].js
@@ -49,18 +49,15 @@ export default function Price() {
 
   const capture = async (id) => {
     const imageSrc = webcamRef.current.getScreenshot();
-    setImageSend(false)
+    setImageSend(false);
     setImgSrc(imageSrc);
     let data_i = JSON.stringify({
       img: imageSrc,
     });
     // Camera End
     setTimeout(() => {
-      setImageSend(true)
-      
+      setImageSend(true);
     }, 5000);
-
-
 
     // Send the captured image to a third-party API
     // try {
@@ -89,7 +86,11 @@ export default function Price() {
         <title>Nextjs | Next-Auth</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${imageSend && styles.active} container-fluid`}>
+      <main
+        className={`${styles.main} ${
+          imageSend && styles.active
+        } container-fluid`}
+      >
         <Image
           className={styles.logo}
           src={CompleteLogo}
@@ -97,9 +98,7 @@ export default function Price() {
           height="40px"
           alt="Complete Logo"
         />
-        <div
-          className="d-flex justify-content-between w-100 px-3"
-        >
+        <div className="d-flex justify-content-between w-100 px-3">
           {/* <TorchContextProvider>
             <TorchControl />
           </TorchContextProvider> */}
@@ -123,13 +122,7 @@ export default function Price() {
               {" "}
               <path d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z" />
             </svg> */}
-            <svg
-              version="1.1"
-              x="0"
-              y="0"
-              viewBox="0 0 24 24"
-              class=""
-            >
+            <svg version="1.1" x="0" y="0" viewBox="0 0 24 24" class="">
               <g>
                 <path
                   fill="#fff"
@@ -144,6 +137,7 @@ export default function Price() {
           </Link>
         </div>
         <div className={styles.cameraContainer}>
+          <div className={styles.scanning_bar} />
           <Webcam
             audio={false}
             ref={webcamRef}
@@ -161,10 +155,18 @@ export default function Price() {
           )} */}
         </div>
 
-        <p className="text-white text-center fw_r" style={{transform: "translateY(20px)"}}>
-          place the cookie in the center <br /> and tap to scan
-        </p>
-        <button onClick={()=>capture(id)}>Scan</button>
+        <div style={{ minHeight: "120px" }}>
+          {imageSend ? (
+            <p className="text-white text-center fw_r pb-2">
+              place the cookie in the center <br /> and tap to scan
+            </p>
+          ) : (
+            <p className="text-white text-center fw_r pb-2">
+              Scanning in process...
+            </p>
+          )}
+          {imageSend && <button onClick={() => capture(id)}>Scan</button>}
+        </div>
 
         <Footer />
       </main>
