c326ae14a561afdd1adf3ac68c3d0790da77ff9b
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-18 16:15:21 +0500

    Instruction tooltip

--- src/pages/prize/[id].js
+++ src/pages/prize/[id].js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 // import styles from "../styles/Home.module.css";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Webcam from "react-webcam";
 
