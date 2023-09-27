5116e4dc75d8da38dc908609c433ee2477a807c8
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-09-27 16:39:48 +0500

    checking the back button

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,34 +2,63 @@ import Head from "next/head";
 // import styles from "../styles/Home.module.css";
 import { getSession, useSession } from "next-auth/react";
 import Nav from "@/components/Nav";
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Drawer from "@/components/Drawer";
 import Link from "next/link";
 
 export default function Home() {
-
+  useEffect(() => {
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    function onBackKeyDown() {
+      //Retrieve app's history
+      var history = App.getHistory();
+  
+      //Check that there's only one screen in history (the current one):
+      if (history.length === 1) {
+        //Check that this element is the default (home) screen:
+        var history_screen = history[0];
+        if (
+          TemplateTags.getDefaultRouteLink().replace("#", "") ===
+          history_screen.fragment
+        ) {
+          //Only one element in history and this element is default screen: exit app on back button:
+          navigator.app.exitApp();
+          return;
+        }
+      }
+  
+      //History has at least one previous element: just go back to it:
+      navigator.app.backHistory();
+    }
+  }, [])
   return (
     <div>
       <Head>
         <title>Oreo | Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://db.onlinewebfonts.com/c/7001cb455721479a34c81bc405ad96d4?family=Pluto+Black" rel="stylesheet" />
+        <link
+          href="https://db.onlinewebfonts.com/c/7001cb455721479a34c81bc405ad96d4?family=Pluto+Black"
+          rel="stylesheet"
+        />
       </Head>
       <main className="container-fluid main float-center flex-column gap-4 px-3">
-      <Nav />
-      <Drawer />
-               
+        <Nav />
+        <Drawer />
+
         <div className="card text-center w-100">
-            <div className="card-header bg-warning text-black fw_b fs-6">
-              WIN THE GRAND PRIZE
-            </div>
-            <div className="card-body">
-              <p className="card-text fw_r">Found Mr Pennybags on the cookie?</p>
-              <p className="card-text fw_r mb-4">Scan it to win a grand prize.</p>
-              <Link href="/grand-prize" className="btn bg-black text-white fw-bold">
-                Scan your Oreo cookie
-              </Link>
-            </div>
+          <div className="card-header bg-warning text-black fw_b fs-6">
+            WIN THE GRAND PRIZE
+          </div>
+          <div className="card-body">
+            <p className="card-text fw_r">Found Mr Pennybags on the cookie?</p>
+            <p className="card-text fw_r mb-4">Scan it to win a grand prize.</p>
+            <Link
+              href="/grand-prize"
+              className="btn bg-black text-white fw-bold"
+            >
+              Scan your Oreo cookie
+            </Link>
+          </div>
         </div>
 
         <div className="card text-center w-100">
@@ -62,6 +91,6 @@ export async function getServerSideProps(context) {
   // }
 
   return {
-    props: { session }
-  }
+    props: { session },
+  };
 }
